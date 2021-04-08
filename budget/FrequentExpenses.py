beffcd219e26fe732da6938d85ba388833775b43
nej789 <nej789@users.noreply.github.com>
2021-04-08 14:17:13 -0700

    Update FrequentExpenses.py
    Added the missing steps.

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -1,17 +1,20 @@
-import collections
-
 from . import Expense
+import collections
+import matplotlib as plt
 
 expenses = Expense.Expenses()
 expenses.read_expenses('data/spending_data.csv')
 
 spending_categories = []
-
 for expense in expenses.list:
     spending_categories.append(expense.category)
 
 spending_counter = collections.Counter(spending_categories)
-
 top5 = spending_counter.most_common(5)
+categories, count = zi;(*top5)
 
+fix, ax = plt.subplots()
+ax.bar(categories, count)
+ax.set_title('# of Purchases by Category')
+plt.show()
 
