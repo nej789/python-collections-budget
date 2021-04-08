f3d1667c6dd83ad1873220c3bdf288f553d224c0
nej789 <nej789@users.noreply.github.com>
2021-04-08 14:01:23 -0700

    Update FrequentExpenses.py

---
+++ budget/FrequentExpenses.py
@@ -0,0 +1,17 @@
+import collections
+
+from . import Expense
+
+expenses = Expense.Expenses()
+expenses.read_expenses('data/spending_data.csv')
+
+spending_categories = []
+
+for expense in expenses.list:
+    spending_categories.append(expense.category)
+
+spending_counter = collections.Counter(spending_categories)
+
+top5 = spending_counter.most_common(5)
+
+
